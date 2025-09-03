f54f9a52825090b7602e5189ca2a95a9df7af160
RupakAryan <157547044+RupakAryan@users.noreply.github.com>
2025-09-03 07:46:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "29978901"
 api_hash = "500fc876c5356cf04ed3698912dc2edf"
-bot_token = "8400419495:AAGjlAjGdNsiRFyv5MmXzl0sWbtdSie_B7o"
+bot_token = "8484698189:AAGISMT50yYSNyUlUivdclcoNjKNOVnCYiY"
 auth_users = [5776977809]
